582520825d7db4584de0c29f6257ce274fbaf20b
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2010-11-08 14:40:42 +0000

    - Import eea.vocab profile only if current profile selected.

--- trunk/eea.vocab/trunk/eea/vocab/setuphandlers.py
+++ trunk/eea.vocab/trunk/eea/vocab/setuphandlers.py
@@ -1,18 +1,24 @@
+""" Setup various
+"""
 def setupATVocabularies(portal):
     """ Installs all AT-based Vocabularies """
-    
+
+    # only run this step if we are in eea.vocab profile
+    if portal.readDataFile('eea.vocab.txt') is None:
+        return
+
     from eea.vocab.atvocabs import vocabs
-    
+
     vkeys = vocabs.keys()
     atvm = portal.getSite().portal_vocabularies
-    
+
     for vkey in vkeys:
 
         print "adding vocabulary %s" % vkey
-        
+
         if hasattr(atvm, vkey):
             continue
-        
+
         atvm.invokeFactory('SimpleVocabulary', vkey)
         vocab = atvm[vkey]
         for (ikey, value) in vocabs[vkey]:
