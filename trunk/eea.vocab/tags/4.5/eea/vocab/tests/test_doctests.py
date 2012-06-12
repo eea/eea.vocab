e567ee3ae9147b4ec36c74fecd1f5605a96f2976
tiberich <tiberich@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-06-12 12:50:05 +0000

    Tagged eea.vocab 4.5.

---
+++ trunk/eea.vocab/tags/4.5/eea/vocab/tests/test_doctests.py
@@ -0,0 +1,20 @@
+""" Doctests
+"""
+import doctest
+import unittest
+from eea.vocab.tests.base import VocabFunctionalTestCase
+from Testing.ZopeTestCase import FunctionalDocFileSuite
+
+OPTIONFLAGS = (doctest.REPORT_ONLY_FIRST_FAILURE |
+               doctest.ELLIPSIS |
+               doctest.NORMALIZE_WHITESPACE)
+
+def test_suite():
+    """ Test suite
+    """
+    return unittest.TestSuite((
+            FunctionalDocFileSuite('README.txt',
+                  optionflags=OPTIONFLAGS,
+                  package='eea.vocab',
+                  test_class=VocabFunctionalTestCase),
+              ))
